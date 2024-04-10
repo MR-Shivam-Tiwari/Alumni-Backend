94662d7ed2c60d1698a0a90e2f4d20769ac7d508
Shivam <shivamt2023@gmail.com>
2024-04-10 17:06:02 +0530

    push

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ wss.on("connection", (connection, req) => {
     const token = tokenCookieString.split("=")[1];
     if (token) {
       console.log('token',token)
-      jwt.verify(token, process.env.secretKey, {}, (err, userData) => {
+      jwt.verify(token, "f3c8a3c9b8a9f0b2440a646f3a5b8f9e6d6e46555a4b2b5c6d7c8d9e0a1b2c3d4f5e6a7b8c9d0e1f2a3b4c5d6e7f8g9h0", {}, (err, userData) => {
         if (err) throw err;
         const { userId, username } = userData;
         connection.userId = userId;
--- routes/alumni.js
+++ routes/alumni.js
@@ -193,7 +193,7 @@ alumniRoutes.post("/login", async (req, res) => {
 
         const token = jwt.sign(
           { userId: alumni._id, username: alumni.firstName },
-          process.env.secretKey
+          "f3c8a3c9b8a9f0b2440a646f3a5b8f9e6d6e46555a4b2b5c6d7c8d9e0a1b2c3d4f5e6a7b8c9d0e1f2a3b4c5d6e7f8g9h0"
         );
         // jwt.sign(
         //   { userId: alumni._id, username: alumni.firstName },
